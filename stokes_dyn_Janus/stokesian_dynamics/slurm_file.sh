9393e7478624e5f4ecaab6b45c183970f86df75d
SoJoes <sphiej223@gmail.com>
2026-02-17 18:08:17 +0000

    setup for longer run

--- stokes_dyn_Janus/stokesian_dynamics/slurm_file.sh
+++ stokes_dyn_Janus/stokesian_dynamics/slurm_file.sh
@@ -27,6 +27,6 @@ python -c "import pyopencl as cl; print(cl.get_platforms())"
 export PYOPENCL_CTX='0'
 
 # Run your script
-python3.11 -O run_simulation.py 11 10 0.1 3 fte
+python3.11 -O run_simulation.py 11 10 0.1 50 fte
 
 python3.11 plotting/plot_positions.py
