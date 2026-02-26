c489133a5e28476132fa49512b35db8d3c13debd
SoJoes <sphiej223@gmail.com>
2026-02-26 11:49:13 +0000

    changed slurm

--- stokes_dyn_Janus/stokesian_dynamics/slurm_short.sh
+++ stokes_dyn_Janus/stokesian_dynamics/slurm_short.sh
@@ -30,5 +30,5 @@ export PYOPENCL_CTX='0'
 
 rm -rf frame_output
 mkdir -p frame_output
-python3.11 -O run_simulation.py 14 10 0.1 10 fte 2
+python3.11 -O run_simulation.py 12 10 0.1 30 fte 2
 python3.11 plotting/plot_positions.py
