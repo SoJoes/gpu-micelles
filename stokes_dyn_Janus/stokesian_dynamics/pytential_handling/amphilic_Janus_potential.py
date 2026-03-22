651dfb31cb1925bd631489af8a4e2ab08d05d8cf
SoJoes <sphiej223@gmail.com>
2026-03-22 14:45:43 +0000

    debug

--- stokes_dyn_Janus/stokesian_dynamics/pytential_handling/amphilic_Janus_potential.py
+++ stokes_dyn_Janus/stokesian_dynamics/pytential_handling/amphilic_Janus_potential.py
@@ -608,14 +608,21 @@ class AmphilicsSolver:
         T_xy = actx.to_numpy(self.T_xy_op(actx, sigma=solution, k=self.k))
         T_yy = actx.to_numpy(self.T_yy_op(actx, sigma=solution, k=self.k))
 
+        T_xx_eval = actx.to_numpy(
+            bind(self.places, self.T_sym[0])(actx, sigma=solution, k=self.k))
+        T_xy_eval = actx.to_numpy(
+            bind(self.places, self.T_sym[1])(actx, sigma=solution, k=self.k))
+        T_yy_eval = actx.to_numpy(
+            bind(self.places, self.T_sym[3])(actx, sigma=solution, k=self.k))
+
         hydro_out = np.array([
             fld.flatten(),
             indicator.flatten(),
             grad_x.flatten(),
             grad_y.flatten(),
-            T_xx.flatten(),
-            T_yy.flatten(),
-            T_xy.flatten()
+            T_xx_eval.flatten(),
+            T_yy_eval.flatten(),
+            T_xy_eval.flatten()
         ], dtype=np.float64)
 
         return (forces_x, forces_y, torques), hydro_out
