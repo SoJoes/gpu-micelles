6f10c7405efefda69ee6f0f974cf01e8e4d65441
SoJoes <sphiej223@gmail.com>
2026-03-24 12:23:58 +0000

    'dirty' caching solution

--- stokes_dyn_Janus/stokesian_dynamics/slurm_short_forloops.sh
+++ stokes_dyn_Janus/stokesian_dynamics/slurm_short_forloops.sh
@@ -32,7 +32,7 @@ export PYOPENCL_COMPILER_OUTPUT='1'
 rm -rf frame_output
 mkdir -p frame_output
 python3.11 -O run_simulation.py 15 10 0.2 2 fte 2 1
-for i in {0..5}; do
+for i in {0..2}; do
     echo "Iteration $i"
     python3.11 -O run_simulation.py 12 10 0.2 2 fte 2 1
 done
