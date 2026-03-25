c017bf57bc605f8831b903095e3750d12c92059b
SoJoes <sphiej223@gmail.com>
2026-03-25 14:16:34 +0000

    test same_setup_as

--- stokes_dyn_Janus/stokesian_dynamics/slurm_short.sh
+++ stokes_dyn_Janus/stokesian_dynamics/slurm_short.sh
@@ -31,5 +31,6 @@ export PYOPENCL_COMPILER_OUTPUT='1'
 
 rm -rf frame_output
 mkdir -p frame_output
-python3.11 -O run_simulation.py 15 10 0.1 5 fte 2 2
+python3.11 -O run_simulation.py 15 10 0.1 5 fte 1 1
+python3.11 -O run_simulation.py 12 10 0.1 5 fte 1 1
 python3.11 plotting/plot_positions.py
