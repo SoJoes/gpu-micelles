fc33df90ed6450ed93c74f8f401854b6cdb6329a
SoJoes <sphiej223@gmail.com>
2026-03-29 16:14:21 +0100

    shear flow

--- stokes_dyn_Janus/stokesian_dynamics/setups/inputs.py
+++ stokes_dyn_Janus/stokesian_dynamics/setups/inputs.py
@@ -265,17 +265,17 @@ def input_ftsuoe(n, posdata, frameno, timestep, last_velocities,
         Ta_in = np.zeros((num_spheres, 3))
         Ta_in[:, 1] = -hydrophobic_forces[2] # need to rotate opposite direction from output
 
+        # Constant shear
+        (Ea_in, U_infinity, O_infinity, centre_of_background_flow,
+         Ot_infinity, Et_infinity) = constant_shear(
+            gammadot=1, frameno=frameno, timestep=timestep,
+            num_spheres=num_spheres)
+
         # added repulsion force
         (Fa_in, Fb_in, DFb_in) = repulsion_forces(
             50, 50, num_spheres, num_dumbbells, sphere_positions,
             dumbbell_positions, dumbbell_deltax, sphere_sizes,
             dumbbell_sizes, num_sphere_in_each_lid, Fa_in, Fb_in, DFb_in)
-
-        # Constant shear
-        (Ea_in, U_infinity, O_infinity, centre_of_background_flow,
-         Ot_infinity, Et_infinity) = constant_shear(
-            gammadot=10, frameno=frameno, timestep=timestep,
-            num_spheres=num_spheres)
 
         desc = "amphilic Janus particles"
 
