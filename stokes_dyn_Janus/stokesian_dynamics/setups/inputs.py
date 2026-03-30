ecf4f2bc170f3ef97ccd3abaec132c26ff00ee5d
SoJoes <sphiej223@gmail.com>
2026-03-30 14:32:59 +0100

    debugging

--- stokes_dyn_Janus/stokesian_dynamics/setups/inputs.py
+++ stokes_dyn_Janus/stokesian_dynamics/setups/inputs.py
@@ -268,7 +268,7 @@ def input_ftsuoe(n, posdata, frameno, timestep, last_velocities,
         # Constant shear
         (Ea_in, U_infinity, O_infinity, centre_of_background_flow,
          Ot_infinity, Et_infinity) = constant_shear(
-            gammadot=1, frameno=frameno, timestep=timestep,
+            gammadot=0.1, frameno=frameno, timestep=timestep,
             num_spheres=num_spheres)
 
         # added repulsion force
