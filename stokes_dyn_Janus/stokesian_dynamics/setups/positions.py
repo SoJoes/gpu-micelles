03b72e777ab4a9e609fc721ef88ff19bdcd4248f
SoJoes <sphiej223@gmail.com>
2026-03-31 10:42:21 +0100

    testing

--- stokes_dyn_Janus/stokesian_dynamics/setups/positions.py
+++ stokes_dyn_Janus/stokesian_dynamics/setups/positions.py
@@ -305,7 +305,7 @@ def pos_setup(n):
 
     elif n==14:
         # roots of unity
-        radius = 1
+        radius = 1.1
         num_spheres = int(np.floor(np.pi/(np.arcsin(1/radius)))) - 3
         num_spheres = 2
 
