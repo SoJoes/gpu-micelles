8147b98f5ea58420704bcfa63fa3f6d10f740df3
SoJoes <sphiej223@gmail.com>
2026-04-06 16:41:09 +0100

    adjusting inputs

--- stokes_dyn_Janus/stokesian_dynamics/pytential_handling/amphilic_Janus_potential.py
+++ stokes_dyn_Janus/stokesian_dynamics/pytential_handling/amphilic_Janus_potential.py
@@ -65,7 +65,7 @@ class AmphilicsSolver:
         )
 
         from sumpy.visualization import FieldPlotter
-        fplot = FieldPlotter(np.zeros(2), extent=40, npoints=700)
+        fplot = FieldPlotter(np.zeros(2), extent=40, npoints=500)
         self.targets = self.actx.from_numpy(fplot.points)
 
         # set up symbolics
