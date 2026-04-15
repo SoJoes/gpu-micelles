b9b604a0c6620d6236c5790dd3e287ed5edc6313
SoJoes <sphiej223@gmail.com>
2026-04-15 10:29:42 +0100

    adjusting inputs

--- stokes_dyn_Janus/stokesian_dynamics/setups/functions_inputs.py
+++ stokes_dyn_Janus/stokesian_dynamics/setups/functions_inputs.py
@@ -72,10 +72,10 @@ def repulsion_forces(strength, tau, num_spheres, num_dumbbells,
         else:
             repulsion_force_length += strength*(2.*a1*a2/(a1+a2))*np.exp(-tau*h_cutoff)'''
 
-        rho1 = 2.2
+        rho1 = 2/5
         norm_r = np.linalg.norm(r)
 
-        if norm_r < rho1:
+        if norm_r -2< rho1:
             repulsion_force_length += np.pi * strength * (np.cos(np.pi * norm_r / (2*rho1))) / (2*rho1)
 
         bead_force[pair[0]] = (np.array(bead_force[pair[0]])
