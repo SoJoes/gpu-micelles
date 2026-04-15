2f0909bb3ac23ee14065dc60ff2a69d2e076303f
SoJoes <sphiej223@gmail.com>
2026-04-15 11:23:34 +0100

    adjusting inputs

--- stokes_dyn_Janus/stokesian_dynamics/slurm_short2.sh
+++ stokes_dyn_Janus/stokesian_dynamics/slurm_short2.sh
@@ -30,5 +30,5 @@ export PYOPENCL_COMPILER_OUTPUT='1'
 
 rm -rf frame_output2
 mkdir -p frame_output2
-python3.11 -u -O run_simulation.py 11 10 0.1 30 fte 1 1 output2 10
-python3.11 plotting/plot_positions.py 1 0 frame_output2 output2
+python3.11 -u -O run_simulation.py 12 10 0.1 40 fte 1 1 output2 10
+python3.11 plotting/plot_positions.py 1 30 frame_output2 output2
