e665e2c050d3b421151983c92e5a0c616ee082ec
SoJoes <sphiej223@gmail.com>
2026-04-16 20:05:04 +0100

    increased repulsion strength

--- stokes_dyn_Janus/stokesian_dynamics/setups/functions_inputs.py
+++ stokes_dyn_Janus/stokesian_dynamics/setups/functions_inputs.py
@@ -80,7 +80,7 @@ def repulsion_forces(strength, tau, num_spheres, num_dumbbells,
         print("No repulsion??", norm_r)
 
         if norm_r - 2 < rho1:
-            repulsion_force_length += np.pi * strength * (np.cos(np.pi * norm_r / (2*rho1))) / (2*rho1)
+            repulsion_force_length -= np.pi * strength * (np.cos(np.pi * norm_r / (2*rho1))) / (2*rho1)
             print("REPULSING!", repulsion_force_length)
 
         bead_force[pair[0]] = (np.array(bead_force[pair[0]])
