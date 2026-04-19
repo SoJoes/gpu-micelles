bc025c6b42a952bee1424480013fa9335bfc66b2
SoJoes <sphiej223@gmail.com>
2026-04-19 17:08:16 +0100

    changing inputs

--- stokes_dyn_Janus/stokesian_dynamics/setups/inputs.py
+++ stokes_dyn_Janus/stokesian_dynamics/setups/inputs.py
@@ -319,7 +319,7 @@ def input_ftsuoe(n, posdata, frameno, timestep, last_velocities,
         # Constant shear
         (Ea_in, U_infinity, O_infinity, centre_of_background_flow,
          Ot_infinity, Et_infinity) = constant_shear(
-            gammadot=0.0025, frameno=frameno, timestep=timestep,
+            gammadot=0.1, frameno=frameno, timestep=timestep,
             num_spheres=num_spheres)
 
         desc = "amphilic Janus particles weak repulsion"
