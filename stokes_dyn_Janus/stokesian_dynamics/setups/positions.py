49228805f5a68fb4f6fda5d218716e5e0331c163
SoJoes <sphiej223@gmail.com>
2026-04-22 19:39:14 +0100

    changing inputs

--- stokes_dyn_Janus/stokesian_dynamics/setups/positions.py
+++ stokes_dyn_Janus/stokesian_dynamics/setups/positions.py
@@ -535,7 +535,7 @@ def pos_setup(n):
 
     elif n == 19:
         # vesicle
-        radius = 17
+        radius = 15
 
         num_outer = 30 #int(np.floor(2*np.pi*radius / 3))
         num_inner = 26 #int(np.floor(2*np.pi*(radius-3.75) / 3))
